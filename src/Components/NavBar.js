e74377ae2a59691ef44749e54c2e3750a5bfad15
Brian MacPherson <brimac1634@gmail.com>
2019-01-13 17:48:12 +0800

    Added navBar and mainView

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+	return (
+		<div className='nav-bar'>
+			<a></a>
+			<a></a>
+			<a></a>
+		</div>
+	);
+}
+
+export default NavBar;
