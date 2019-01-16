c8af4d4530e9befa176e5cd39678cd12689912f8
Brian MacPherson <brimac1634@gmail.com>
2019-01-16 17:57:08 +0800

    fixed some styling

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,15 +5,17 @@ import Icon from '../Icon/Icon';
 const SearchBar = ( {searchField, searchChange} ) => {
 	return (
 		<div className='search-bar'>
-			<div className='search-container'>
-				<input 
-					className='search' 
-					type='search' 
-					placeholder='English/Cantonese/Mandarin/Jyutping' 
-					onChange={searchChange}
-				/>
-				<Icon iconStyle='dark' icon='search' width='30'/>
-			</div>
+				<div className='search-container'>
+					<input 
+						className='search' 
+						type='search' 
+						placeholder='English/Cantonese/Mandarin/Jyutping' 
+						onChange={searchChange}
+					/>
+					<div className='icon-container'>
+						<Icon className='search-icon' iconStyle='dark' icon='search' width='30'/>
+					</div>
+				</div>
 			<div className='bottom-divider'></div>
 		</div>
 	);
