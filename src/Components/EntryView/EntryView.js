462d9a4302f4f2c0e36d480c8cdae89045880524
Brian MacPherson <brimac1634@gmail.com>
2019-02-15 20:01:14 +0800

    made favorite button pink if favorited

--- src/Components/EntryView/EntryView.js
+++ src/Components/EntryView/EntryView.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './EntryView.css';
 import Icon from '../Icon/Icon';
 
-const EntryView = (props) => {
-	const {
-		userID,
-		entry,
-		entry: {
-			entryID,
-			cantoword,
-			classifier,
-			jyutping,
-			englishword,
-			mandarinword,
-			cantosentence,
-			jyutpingsentence,
-			englishsentence
-		},
-		updateEntries,
-		updateSelected
-	} = props;
-
-	let clLabel = 'cl: ';
-	let color = ''
-
-	if (!classifier) {
-		clLabel = '';
+class EntryView extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isFavorited: false
+		}
 	}
 
-	const renderLikeButton = (entryID, userID) => {
-		console.log(entryID, userID);
+	render() {
+		const {
+			userID,
+			entry,
+			entry: {
+				entryID,
+				cantoword,
+				classifier,
+				jyutping,
+				englishword,
+				mandarinword,
+				cantosentence,
+				jyutpingsentence,
+				englishsentence
+			},
+			updateEntries,
+			updateSelected
+		} = this.props;
+
+		const { isFavorited } = this.state;
+
+		let clLabel = 'cl: ';
+
+		if (!classifier) {
+			clLabel = '';
+		}
+
 		if (entryID !== undefined && userID !== '') {
 			fetch('http://localhost:3000/Favorites/isFavorited', {
 				method: 'post',
@@ -40,94 +47,106 @@ const EntryView = (props) => {
 				})
 			})
 				.then(data => data.json())
-				.then(isFavorited => {
-					color = isFavorited ? 'cantoPink' : 'cantoDarkBlue'
+				.then(favorited => {
+					if (favorited !== isFavorited) {
+						this.setState({isFavorited: favorited})
+					}
 				})
 				.catch(err => console.log('Unable to check favorite'))
 		}
-	}
 
-	const toggleFavorite = (entryID, userID, cantoWord) => {
-		fetch('http://localhost:3000/Favorites/toggle', {
-			method: 'post',
-			headers: {'Content-Type': 'application/json'},
-			body: JSON.stringify({
-				entryid: entryID,
-				userid: userID,
-				cantoword: cantoWord
-			})
-		})
-			.then(data => data.json())
-			.then(result => {
-				if (result === 'deleted') {
-					updateEntries(userID)
-					updateSelected('')
-				}
-			})
-			.catch(err => console.log('Unable to toggle favorite'))
-	}
+		const toggleFavorite = (entryID, userID, cantoWord) => {
+			if (userID !== '') {
+				fetch('http://localhost:3000/Favorites/toggle', {
+					method: 'post',
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify({
+						entryid: entryID,
+						userid: userID,
+						cantoword: cantoWord
+					})
+				})
+					.then(data => data.json())
+					.then(result => {
+						if (result === 'deleted') {
+							updateEntries(userID)
+							updateSelected('')
+						}
+					})
+					.catch(err => console.log('Unable to toggle favorite'))
+			} else {
+				//present pop up "you must be signed in to favorite words"
+			}
+		}
 
-	renderLikeButton(entryID, userID);
-
-	return (
-		<div className='entry-view'>
-			{entry !== ''
-				?   <div className='inner-entry-view'>
-						<div className='entry-btn-container'>
-							<button className='entry-btn' onClick={() => toggleFavorite(entryID, userID, cantoword)}>
-								<Icon 
-									icon='like-2' 
-									width='35' 
-									iconStyle='dark'
-									color={color}
-								/>
-							</button>
-							<button className='entry-btn'>
-								<Icon icon='speaker-5' width='35' iconStyle='dark'/>
-							</button>
-						</div>
-						<div className='top-group'>
-							<div className='canto-class'>
-								<h3>{cantoword}</h3>
-								<p>{clLabel}{classifier}</p>
+		return (
+			<div className='entry-view'>
+				{entry !== ''
+					?   <div className='inner-entry-view'>
+							<div className='entry-btn-container'>
+								<button 
+									className='entry-btn' 
+									onClick={() => toggleFavorite(entryID, userID, cantoword)}
+								>
+									<Icon 
+										icon='like-2' 
+										width='35' 
+										iconStyle='dark'
+										color={
+											isFavorited
+											? 'cantoPink'
+											: 'cantoDarkBlue'
+										}
+									/>
+								</button>
+								<button className='entry-btn'>
+									<Icon icon='speaker-5' width='35' iconStyle='dark'/>
+								</button>
 							</div>
-							<div><p>{jyutping}</p></div>
+							<div className='top-group'>
+								<div className='canto-class'>
+									<h3>{cantoword}</h3>
+									<p>{clLabel}{classifier}</p>
+								</div>
+								<div><p>{jyutping}</p></div>
+							</div>
+							<div>
+								<div><p>En: {englishword}</p></div>
+								<div><p>普: {mandarinword}</p></div>
+							</div>
+							<div>
+								<div><p>{cantosentence}</p></div>
+								<div><p>{jyutpingsentence}</p></div>
+								<div><p>{englishsentence}</p></div>
+							</div>
 						</div>
-						<div>
-							<div><p>En: {englishword}</p></div>
-							<div><p>普: {mandarinword}</p></div>
-						</div>
-						<div>
-							<div><p>{cantosentence}</p></div>
-							<div><p>{jyutpingsentence}</p></div>
-							<div><p>{englishsentence}</p></div>
-						</div>
-					</div>
-				:   <div className='inner-entry-view'>
-						<div className='entry-btn-container'>
-							<div className='ghost-div'/>
-							<div className='ghost-div'/>
-						</div>
-						<div className='top-group'>
-							<div className='canto-class'>
+					:   <div className='inner-entry-view'>
+							<div className='entry-btn-container'>
+								<div className='ghost-div'/>
+								<div className='ghost-div'/>
+							</div>
+							<div className='top-group'>
+								<div className='canto-class'>
+									<div className='ghost-div'>&nbsp;</div>
+									<div className='ghost-div'>&nbsp;</div>
+								</div>
+								<div className='ghost-div'>&nbsp;</div>
+							</div>
+							<div>
 								<div className='ghost-div'>&nbsp;</div>
 								<div className='ghost-div'>&nbsp;</div>
 							</div>
-							<div className='ghost-div'>&nbsp;</div>
-						</div>
-						<div>
-							<div className='ghost-div'>&nbsp;</div>
-							<div className='ghost-div'>&nbsp;</div>
-						</div>
-						<div>
-							<div className='ghost-div'>&nbsp;</div>
-							<div className='ghost-div'>&nbsp;</div>
-							<div className='ghost-div'>&nbsp;</div>
-						</div>
-					</div>}
-		</div>
-		
-	);
+							<div>
+								<div className='ghost-div'>&nbsp;</div>
+								<div className='ghost-div'>&nbsp;</div>
+								<div className='ghost-div'>&nbsp;</div>
+							</div>
+						</div>}
+			</div>
+			
+		);
+	}
+	
 }
 
 export default EntryView;
