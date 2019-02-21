29210a24caf75edab0ffdb6cf3889c237125b462
Brian MacPherson <brimac1634@gmail.com>
2019-02-21 16:51:24 +0800

    added validation logic to login and register

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -34,43 +34,63 @@ class SignIn extends Component {
 	onEmailChange = (event) => this.setState({ email: event.target.value })
 	onPasswordChange = (event) => this.setState({ password: event.target.value })
 
+	validateEmail = (email) => {
+		const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return regexp.test(email);
+	}
+
+	validatePassword = (password) => {
+		return password.length >= 6 ? true : false;
+	}
+
 	onUserSubmit = () => {
 		const { updateUser, removePopUpBegin } = this.props;
 		const { title, email, password } = this.state;
-		if (title === 'Login') {
-			//login
-			fetch('http://localhost:3000/signin', {
-				method: 'post',
-				headers: {'Content-Type': 'application/json'},
-				body: JSON.stringify({
-					email: email,
-					password: password
-				})
-			})
-				.then(res => res.json())
-				.then(user => {
-					console.log(user)
-					updateUser(user)
-					removePopUpBegin()
-				})
-
+		const emailIsValid = validateEmail(email);
+		const passwordIsValid = validatePassword(password);
+		if (!emailIsValid && !passwordIsValid) {
+			//give alert that there is missing info
+		} else if (!emailIsValid) {
+			//give alert that email is incorrect
+		} else if (!passwordIsValid) {
+			//give alert that password must contain 6 characters
 		} else {
-			//register
-			fetch('http://localhost:3000/register', {
-				method: 'post',
-				headers: {'Content-Type': 'application/json'},
-				body: JSON.stringify({
-					email: email,
-					password: password
+			if (title === 'Login') {
+				//login
+				fetch('http://localhost:3000/signin', {
+					method: 'post',
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify({
+						email: email,
+						password: password
+					})
 				})
-			})
-				.then(res => res.json())
-				.then(user => {
-					updateUser(user)
-					removePopUpBegin()
+					.then(res => res.json())
+					.then(user => {
+						console.log(user)
+						updateUser(user)
+						removePopUpBegin()
+					})
+
+			} else {
+				//register
+				fetch('http://localhost:3000/register', {
+					method: 'post',
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify({
+						email: email,
+						password: password
+					})
 				})
-				.catch(console.log)
+					.then(res => res.json())
+					.then(user => {
+						updateUser(user)
+						removePopUpBegin()
+					})
+					.catch(console.log)
+			}
 		}
+		
     }
 
 	render() {
