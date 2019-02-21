823d8c9544fb723802a6894fb2bcd27030c7ee6b
Brian MacPherson <brimac1634@gmail.com>
2019-02-21 14:18:35 +0800

    added info pop about cantotalk

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import CantoIcon from './CantoTalkIconBlue.png';
 
-const Logo = () => {
+const Logo = ({ iconSize }) => {
+	let height = (iconSize) ? iconSize : '100%';
 	return (
-		<img src={CantoIcon} height='100%' width='auto' alt='CantoTalk Icon'/>
+		<img src={CantoIcon} height={height} width='auto' alt='CantoTalk Icon'/>
 	);
 }
 
--- src/Components/LogoFull/LogoFull.js
+++ src/Components/LogoFull/LogoFull.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './LogoFull.css';
 import Logo from '../Logo/Logo';
 
-const LogoFull = () => {
+const LogoFull = ({ iconSize, fontSize }) => {
+	let defaultFontSize = (fontSize) ? fontSize : '1.17em';
+	let defaultIconSize = (iconSize) ? iconSize : '30px';
 	return (
-		<div className='logo-full'>
-			<Logo />
-			<h3>CantoTalk</h3>
+		<div className='logo-full' >
+			<Logo iconSize={defaultIconSize}/>
+			<h3 style={{fontSize: `${defaultFontSize}`}}>CantoTalk</h3>
 		</div>
 	);
 }
