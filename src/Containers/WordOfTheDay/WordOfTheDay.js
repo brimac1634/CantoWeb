b59f1a4258b8fc49ccae9baf68e46c7077ea83ef
Brian MacPherson <brimac1634@gmail.com>
2019-05-29 14:52:35 +0800

    started to add word of day page

--- src/Containers/WordOfTheDay/WordOfTheDay.js
+++ src/Containers/WordOfTheDay/WordOfTheDay.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './WordOfTheDay.css';
+import EntryView from '../../Components/EntryView/EntryView';
+import SearchBar from '../../Components/SearchBar/SearchBar';
+import MediaQuery from 'react-responsive';
 
-const WordOfTheDay = () => {
-	return (
-		<div className='page word-of-day center-div'>
-			<h2>Coming Soon!</h2>
-		</div>
-	);
+class WordOfTheDay extends Component {
+	constructor(props) {
+		super()
+		this.state = {
+			entries: [],
+			selectedEntry: {}
+		}
+	}
+
+	render() {
+		const { selectedEntry } = this.state;
+		return (
+			<div className='page word-of-day'>
+				<MediaQuery minWidth={700}>
+					<div className='split-container'>
+						<div className='half-container'>
+							hey
+						</div>
+						<div className='half-container'>
+							<EntryView 
+								selectedEntry={selectedEntry} 
+							/>
+						</div>
+					</div>
+				</MediaQuery>
+				<MediaQuery maxWidth={699}>
+					<div className='split-container'>
+						<div className='half-container'>
+							<EntryView 
+								selectedEntry={selectedEntry} 
+							/>
+						</div>
+					</div>
+					<SearchBar 
+						className='search-bar'
+					/>
+				</MediaQuery>
+			</div>
+		);
+	}
 }
 
 export default WordOfTheDay;
